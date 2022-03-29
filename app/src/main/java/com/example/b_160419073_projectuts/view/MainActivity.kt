0ab6639b6541bc7d16d30e799c910504310fcde1
adamt <PieceOfCloth>
2022-03-29 22:47:34 +0700

    day1_290322

--- app/src/main/java/com/example/b_160419073_projectuts/view/MainActivity.kt
+++ app/src/main/java/com/example/b_160419073_projectuts/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.b_160419073_projectuts
+package com.example.b_160419073_projectuts.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.b_160419073_projectuts.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
