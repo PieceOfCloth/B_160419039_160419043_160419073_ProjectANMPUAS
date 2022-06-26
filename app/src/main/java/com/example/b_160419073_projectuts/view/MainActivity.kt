4a41ef60c8058f8c908606ce8f30121978211976
BryanD <vercelli.bryan92@gmail.com>
2022-06-26 23:16:04 +0700

    test

--- app/src/main/java/com/example/b_160419073_projectuts/view/MainActivity.kt
+++ app/src/main/java/com/example/b_160419073_projectuts/view/MainActivity.kt
@@ -11,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController1: NavController
-
+    //bryan was here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
