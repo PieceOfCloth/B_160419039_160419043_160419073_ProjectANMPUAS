122d3b69e048ab33e13971d9df0078e983cec4ec
BryanD <vercelli.bryan92@gmail.com>
2022-06-27 13:42:25 +0700

    Data binding home

--- app/src/main/java/com/example/b_160419073_projectuts/view/ListKosAdapter.kt
+++ app/src/main/java/com/example/b_160419073_projectuts/view/ListKosAdapter.kt
@@ -3,25 +3,30 @@ package com.example.b_160419073_projectuts.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.b_160419073_projectuts.R
+import com.example.b_160419073_projectuts.databinding.ListKosItemBinding
 import com.example.b_160419073_projectuts.loadImage
 import com.example.b_160419073_projectuts.model.Kos
 import kotlinx.android.synthetic.main.list_kos_item.view.*
 
 class ListKosAdapter(val listKos:ArrayList<Kos>):RecyclerView.Adapter<ListKosAdapter.KosViewHolder>(){
-    class KosViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class KosViewHolder(var view: ListKosItemBinding) : RecyclerView.ViewHolder(view.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KosViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.list_kos_item, parent, false)
+        //val view = inflater.inflate(R.layout.list_kos_item, parent, false)
+        val view = DataBindingUtil.inflate<ListKosItemBinding>(inflater,
+            R.layout.list_kos_item, parent, false)
 
         return KosViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: KosViewHolder, position: Int) {
-        holder.view.txtId.text = listKos[position].id
+        holder.view.kos = listKos[position]
+        /*holder.view.txtId.text = listKos[position].id
         holder.view.txtNamaKos.text = listKos[position].nama_kos
         holder.view.txtAlamat.text = listKos[position].alamat
         holder.view.txtRangeHarga.text = listKos[position].range_harga
@@ -30,7 +35,7 @@ class ListKosAdapter(val listKos:ArrayList<Kos>):RecyclerView.Adapter<ListKosAda
         holder.view.btnDetail.setOnClickListener {
             val action = HomeFragmentDirections.actionDetailKos(holder.view.txtId.text.toString())
             Navigation.findNavController(it).navigate(action)
-        }
+        }*/
     }
 
     override fun getItemCount(): Int {
